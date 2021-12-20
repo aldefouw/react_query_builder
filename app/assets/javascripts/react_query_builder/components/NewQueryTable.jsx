4786e7259efd06724fddf7331a48d27cd215b293
Adam De Fouw <aldefouw@medicine.wisc.edu>
2021-12-20 11:12:22 -0600

    Consolidate tables

--- app/assets/javascripts/react_query_builder/components/NewQueryTable.jsx
+++ app/assets/javascripts/react_query_builder/components/NewQueryTable.jsx
@@ -1,12 +1,25 @@
-class NewQueryTable extends BaseTable {
+class NewQueryTable extends React.Component {
 
     constructor(props) {
         super(props)
+
+        this.columns = []
+
+        this.state = {
+            data: [],
+            loading: true
+        }
+
         this.setColumns(props.cols)
 
-        this.match_date_time = new MatchDate('MM/DD/YYYY HH:mm:ss', 'YYYY-MM-DD HH:mm:ss +0000')
-        this.match_date = new MatchDate('MM/DD/YYYY', 'YYYY-MM-DD')
-        this.match_time = new MatchTime('HH:mm:ss', 'YYYY-MM-DD HH:mm:ss +0000')
+        this.match_timestamp = new MatchDate('MM/DD/YYYY h:mmA', 'YYYY-MM-DDTHH:mm:ss +0000')
+        this.match_date_time = new MatchDate('MM/DD/YYYY h:mmA', 'YYYY-MM-DDTHH:mm:ss +0000')
+        this.match_date = new MatchDate('MM/DD/YYYY')
+        this.match_time = new MatchTime('HH:mm:ss')
+    }
+
+    lazyLoadData(data){
+        this.setState({ data: data, loading: false });
     }
 
     setColumns(cols){
@@ -42,5 +55,39 @@ class NewQueryTable extends BaseTable {
         }
 
         this.columns = this.columns.concat( cols.map( col => newColumn(col)) )
+    }
+
+    render() {
+        const { data, loading } = this.state;
+
+        const filterCaseInsensitiveSubString = (filter, row) => {
+            const id = filter.pivotId || filter.id;
+            if (row[id] !== null && typeof row[id] === 'string') {
+                return (
+                    row[id] !== undefined ?
+                        String(row[id].toLowerCase()).includes(filter.value.toLowerCase()) : true
+                )
+            }
+            else {
+                return (
+                    String(row[filter.id]).includes(filter.value)
+                )
+            }
+        }
+
+        return (
+            <ReactTable
+                data={data}
+                loading={loading}
+                columns={this.columns}
+                showFilters={true}
+                filterable
+                minRows={3}
+                defaultFilterMethod={filterCaseInsensitiveSubString}
+                defaultPageSize={25}
+                defaultSorted={this.props.defaultSorted}
+                className="-striped -highlight"
+            />
+        )
     }
 }
