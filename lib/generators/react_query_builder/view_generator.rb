c214a1c37e66b5d7f3c70ccf825eb4b7c6e4a932
Adam De Fouw <aldefouw@medicine.wisc.edu>
2021-12-28 10:31:36 -0600

    Create view generator

---
+++ lib/generators/react_query_builder/view_generator.rb
@@ -0,0 +1,13 @@
+require "rails/generators"
+
+module ReactQueryBuilder
+	module Generators
+		class ViewGenerator < ::Rails::Generators::Base
+
+			def create
+				generate "scenic:view qb_#{args[0]}"
+			end
+
+		end
+	end
+end
