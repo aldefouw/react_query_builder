37736821c7aa5677cd2bea8d61fdb77b4973a49a
Adam De Fouw <aldefouw@medicine.wisc.edu>
2022-02-22 13:21:01 -0600

    Make adjustments to Assets to make things more flexible

--- spec/dummy/app/assets/javascripts/react_query_builder.js
+++ spec/dummy/app/assets/javascripts/react_query_builder.js
@@ -1,3 +1,27 @@
+// ReactQueryBuilder Javascript
+//= require jquery-ui
+
+// AJAX requests
+//= require axios/dist/axios
+
+// Handle dates gracefully
+//= require moment/moment
+
+// Type-ahead functionality
+//= require chosen-js
+
+// For Tool Tips
+//= require popper
+//= require bootstrap
+
+// React Table Dependencies
+//= require react
+//= require react-table/react-table
+//= require react-csv
+
+// Downloading XLS files on the fly
+//= require xlsx
+
 // Uncomment the appropriate one below to match your environment
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -5,5 +29,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 })
 
 // document.addEventListener("turbolinks:load", function(event) {
-//	ReactQueryBuilder.enable()
+//   ReactQueryBuilder.enable()
 // })
