8022a8368433b6441a99931bb470abcb15ca7e4e
Adam De Fouw <aldefouw@medicine.wisc.edu>
2022-02-28 13:15:14 -0600

    Namespace the helper files | Issue #1

---
+++ app/helpers/react_query_builder/query_builder_display_helper.rb
@@ -0,0 +1,58 @@
+module ReactQueryBuilder
+
+  # -------------------------------
+  #    QueryBuilderDisplayHelper
+  # -------------------------------
+  # => Display fields (checkboxes)
+  # => Results table (React table)
+
+  module QueryBuilderDisplayHelper
+
+    # Accepts a list of model attributes & returns hash of attribute and label for checkboxes
+    def display_fields
+      select_list.map { |x| { attribute_name: x.first, label: x.second } }
+    end
+
+    def select_list
+      if @cols.present? && params[:action] != "edit"
+        posted_columns(@cols)
+      elsif params[:action] == "edit"
+        posted_columns(JSON.parse(@query.display_fields).keys)
+      else
+        existing_columns
+      end
+    end
+
+    def posted_columns(cols)
+      options = {}
+      cols.each { |c| options[c] = @report.labels[c] }
+      @report.labels.keys.each { |l| options[l] = @report.labels[l] unless cols.include?(l) }
+      options
+    end
+
+    def existing_columns
+      @report.labels
+    end
+
+    def display_select(items)
+      select_tag(:display_fields,
+                 options_for_select(items.map{ |s| [s[:label], s[:attribute_name]] }, selected: selected),
+                 multiple: true,
+                 class: "chosen-select chosen-sortable")
+    end
+
+    def selected
+      if params[:action] == "edit"
+        JSON.parse(@query.display_fields).keys
+      elsif JSON.parse(@query.display_fields).map(&:first).count > 0
+        JSON.parse(@query.display_fields).map(&:first)
+      elsif params[:action] == "new"
+        @report.labels.keys #Show all when you come to a new query
+      else
+        params[:display_fields]
+      end
+    end
+
+  end
+
+end
---
+++ app/helpers/react_query_builder/query_builder_index_helper.rb
@@ -0,0 +1,52 @@
+module ReactQueryBuilder
+
+  # -------------------------------
+  #     QueryBuilderIndexHelper
+  # -------------------------------
+  # => Index age/query manager user interface
+  # => Saved query tables
+
+  module QueryBuilderIndexHelper
+
+    # ***********************************************
+    #   QUERY MANAGER/INDEX PAGE/SAVED QUERY TABLES
+    # ***********************************************
+
+    # => A single row of a saved query table on index page
+    def saved_query_index_table_row(query)
+      td_toolbar = query_action_toolbar(query)
+      td_title = content_tag(:td, query.title)
+      td_description = content_tag(:td, query.description)
+      td_last_run_date = content_tag(:td, query.last_run)
+      td_last_run_by = content_tag(:td, query.last_run_by)
+      content_tag(:tr, td_title + td_description + td_last_run_date + td_last_run_by + td_toolbar)
+    end
+
+    # => Button toolbar for a saved query on index page
+    #     - 'Run query' button and 'delete query' button
+    def query_action_toolbar(query)
+      run_button = link_to "<i class=\"fas fa-bolt\"></i>&nbsp;Run".html_safe,
+                           react_query_builder_rails_engine.query_builder_path(id: query.id),
+                           class: 'btn btn-sm btn-info mr-1 rqb_btn rqb_run_btn '
+
+      edit_button = link_to "<i class=\"fas fa-edit\"></i>&nbsp;Edit".html_safe,
+                            react_query_builder_rails_engine.edit_query_builder_path(id: query.id),
+                            class: 'btn btn-sm btn-primary mr-1 rqb_btn rqb_edit_btn'
+
+      delete_button = link_to "<i class=\"fas fa-trash-alt\"></i>".html_safe,
+                              react_query_builder_rails_engine.query_builder_path(id: query.id),
+                              method: :delete,
+                              data: {confirm: "Are you sure you want to delete this query?"},
+                              class: 'btn btn-sm btn-danger rqb_btn rqb_delete_btn'
+
+      button_div = content_tag(:div, run_button + edit_button + delete_button)
+      td_toolbar = content_tag(:td, button_div)
+    end
+
+    def selected_query(index, report)
+      index == 0 && !params.key?(:query_type) || (params.key?(:query_type) && report[:model].underscore == params[:query_type])
+    end
+
+  end
+
+end
