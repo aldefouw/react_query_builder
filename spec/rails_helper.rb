dd883cd6c5faa5855c6f305cd6e00d734cdc7aeb
Adam De Fouw <aldefouw@medicine.wisc.edu>
2022-03-01 08:37:22 -0600

    Include every dependency necessary explicitly | Issue #4

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,23 @@
 #rails_helper.rb
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'spec_helper'
+
+require 'rails/all'
+
+require 'pry'
+
+require 'bootstrap'
+require 'jquery-rails'
+require 'jquery-ui-rails'
+require 'react-rails'
+require 'simple_form'
+require 'reform/rails'
+
+require 'react_query_builder'
+
 require File.expand_path('../dummy/config/environment.rb', __FILE__)
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
