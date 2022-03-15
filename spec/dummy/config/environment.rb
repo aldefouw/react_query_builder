dc40769948a27030b02696cacbf527be1506453b
Adam De Fouw <aldefouw@medicine.wisc.edu>
2022-03-15 14:01:48 -0500

    Mock current_user for test app | Issue #4

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -3,3 +3,11 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ReactQueryBuilder::QueryBuilderController.class_eval do
+
+	def current_user
+		Person.first
+	end
+
+end
